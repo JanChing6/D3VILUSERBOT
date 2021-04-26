63cc6c3bb081c568136f6d1a1e60e27fe3e04114
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-04-26 10:02:09 +0530

    Update savage.py

--- userbot/savage.py
+++ userbot/savage.py
@@ -5,18 +5,18 @@
 #chala jaa bhosdike 
 import os
 from userbot import ALIVE_NAME 
-MASTER = str(ALIVE_NAME) if ALIVE_NAME else "SAVAGE BOY"
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "D3VIL BOY"
 NAME = os.environ.get("BOT_NICK_NAME")
-BOT =  = str(NAME) if NAME else "SAVAGE BOT"
+BOT =  = str(NAME) if NAME else "D3VIL BOT"
 #op for keinshin
 
-SAMEER = "[SAMEER](https://t.me/sameer_795)"
-OP = "[SAVAGE BOT](https://github.com/sameerpanthi/SAVAGE-IS-BACK)"
-OKAY = "[SUPPORT GROUP](https://t.me/savage_team_userbot)"
+SAMEER = "[SAMEER](https://t.me/D3_krish)"
+OP = "[SAVAGE BOT](https://github.com/D3KRISH/D3VILUSERBOT)"
+OKAY = "[SUPPORT GROUP](https://t.me/D3VIL_BOT_SUPPORT)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
-ALIVE = "SAVAGE BOT IS ON🔥 FIRE 🔥" #make by LEGENDX22 for SAVAGE BOT
-USERBOT = " HELLO MASTER MY NAME IS SAVAGE BOT I AM A BEST USERBOT 💝"
+ALIVE = "D3VIL BOT IS ON🔥 FIRE 🔥" #make by LEGENDX22 for D3VIL BOT
+USERBOT = " HELLO MASTER MY NAME IS D3VIL BOT I AM A BEST USERBOT 💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
 #aur aage add kruga abhi busy okay 🤔
