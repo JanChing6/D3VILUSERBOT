f40c053a696a23969b4b05c7ec61c83bd730f7fb
D3_krish <83129000+D3KRISH@users.noreply.github.com>
2021-04-26 11:09:46 +0530

    Update thunderconfig.py

--- userbot/thunderconfig.py
+++ userbot/thunderconfig.py
@@ -85,20 +85,20 @@ class Config(object):
     SPOTIFY_BIO_PREFIX = os.environ.get("SPOTIFY_BIO_PREFIX", None)
     ASSISTANT_LOG = int(os.environ.get("ASSISTANT_LOG", False))
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/sameerpanthi/SAVAGE-IS-BACK"
+        "UPSTREAM_REPO", "https://github.com/D3KRISH/D3VILUSERBOT"
     )
     ALIVE_PIC = os.environ.get(
-        "ALIVE_IMAGE", "https://telegra.ph/file/63abc60224dc567e3d441.jpg"
+        "ALIVE_IMAGE", "https://telegra.ph/file/9c906813b9388046b6984.jpg"
     )
     CUSTOM_ALIVE_PIC = os.environ.get(
-        "CUSTOM_ALIVE_IMG", "https://telegra.ph/file/50e422f6b07fa9126c1d1.jpg"
+        "CUSTOM_ALIVE_IMG", "https://telegra.ph/file/9c906813b9388046b6984.jpg"
     )
     ALIVE_IMAGE = os.environ.get(
-        "ALIVE_PIC", "https://telegra.ph/file/63abc60224dc567e3d441.jpg"
+        "ALIVE_PIC", "https://telegra.ph/file/9c906813b9388046b6984.jpg"
     )
     ASSISTANT_START_PIC = os.environ.get(
         "ASSISTANT_START_PIC",
-        "https://telegra.ph/file/63abc60224dc567e3d441.jpg",
+        "https://telegra.ph/file/9c906813b9388046b6984.jpg",
     )
     TESSDATA_PREFIX = os.environ.get(
         "TESSDATA_PREFIX", "/usr/share/tesseract-ocr/4.00/tessdata"
